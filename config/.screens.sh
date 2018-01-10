efee518fe9ff999fbe1aa9e4ebb2535ab07f3847
root <root@localhost.localdomain>
2018-01-10 08:49:12 -0200

    Added dotfiles for real now

---
+++ config/.screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-1-1 --off --output DP-1-1 --off --output VGA-1-1 --mode 1280x1024 --pos 0x112 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1280x0 --rotate normal --output DP-1 --off --output DP-0 --off
