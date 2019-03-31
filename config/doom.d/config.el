35ce3df6d91c843e708e7c28538d490ce96f5eed
Rodrigo Leite <rodrigolf.dev@gmail.com>
2019-03-31 19:39:26 -0400

    Bunch of stuff

--- config/doom.d/config.el
+++ config/doom.d/config.el
@@ -1,13 +1,20 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-
 (after! company
   (setq company-idle-delay 0.2
         company-minimum-prefix-length 3))
 
+(after! web-mode
 (after! tide
-  (setq tide-tsserver-executable "node_modules/typescript/bin/tsserver"))
+    (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+    (add-hook 'web-mode-hook
+            (lambda ()
+                (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                (setup-tide-mode))))
+    ;; enable typescript-tslint checker
+    (flycheck-add-mode 'typescript-tslint 'web-mode))
+)
 
 (map! :leader
       :prefix ("o" . "open")
